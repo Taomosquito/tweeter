7529c07f8f4c583d6b73d304cc3fce46ef2bf2b3
Taomosquito <S_Peloquin123@live.com>
2024-07-09 21:56:00 -0400

    complete dynamic tweets behavior

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,70 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetDataObjects = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1720371217904
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1720487527136
+  }
+];
 
+
+$(document).ready(function() {
+
+  const $tweetsContainer = $('#tweets-container');
+
+  const createTweetElement = function(randomObject) {
+    let $tweet = $(`
+    <article class="tweet">
+    <header>
+    <div class="tweet-topLeft">
+    <img src="${randomObject.user.avatars}" alt="${randomObject.user.name}'s avatar" />  
+    <p>${randomObject.user.name}</p>
+    </div>
+      <p class="greyOut">${randomObject.user.handle}</p>
+    </header>
+    <section>
+    ${randomObject.content.text}
+    </section>
+    <hr />
+    <footer>
+      <p>created: ${randomObject.created_at}</p>
+      <footer class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </footer>
+    `);
+
+    return $tweet;
+  };
+
+  const renderTweets = function(randomObjectArr) {
+    for (singleObject of randomObjectArr) {
+      let $renderedTweet = createTweetElement(singleObject);
+      $tweetsContainer.append($renderedTweet);
+    }
+  }
+
+  renderTweets(tweetDataObjects);
+
+  // }
+});
