78e47fa540eb14e5b8eb68fae7ddc5dc84a16074
Taomosquito <S_Peloquin123@live.com>
2024-07-12 18:51:40 -0400

    fetch tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,37 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetDataObjects = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1720371217904
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1720487527136
-  }
-];
-
 
 $(document).ready(function() {
-
   const $tweetsContainer = $('#tweets-container');
 
   const createTweetElement = function(randomObject) {
+    const formattedTime = timeago.format(randomObject.created_at);
     let $tweet = $(`
     <article class="tweet">
     <header>
@@ -48,7 +23,7 @@ $(document).ready(function() {
     </section>
     <hr />
     <footer>
-      <p>created: ${randomObject.created_at}</p>
+      <p>created: ${formattedTime}</p>
       <footer class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -57,33 +32,40 @@ $(document).ready(function() {
     `);
 
     return $tweet;
-  };
+  }
 
   const renderTweets = function(randomObjectArr) {
     console.log("Random Object Array: ", randomObjectArr);
     for (singleObject of randomObjectArr) {
       let $renderedTweet = createTweetElement(singleObject);
-      $tweetsContainer.append($renderedTweet);
+      $tweetsContainer.prepend($renderedTweet);
     }
   }
 
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (serverData) => {
+        console.log(serverData);
+        renderTweets(serverData);
+      }
+    })
+  }
 
   const $form = $("#tweet-prompt");
 
   $form.on("submit", function(event) {
     event.preventDefault();
-    const tweetData = $form.serialize();;
+    const tweetData = $form.serialize();
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: tweetData,
       success: (response) => {
-        console.log(response)
-        renderTweets(response)
+        console.log(response);
+        loadTweets(response);
       }
     })
-
   })
-
-  // renderTweets(tweetDataObjects);
 })
